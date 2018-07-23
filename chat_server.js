0d8f66d58a7590d5038052a0296ad5fe0b1db717
ipointer2 <chris.e.frank@gmail.com>
2018-07-23 15:52:57 -0400

    get chat works

--- chat_server.js
+++ chat_server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const mime = require('mime-types');
 const Assistant = require('./assistant');
 const port = process.env.PORT || 5000;
+let messages = [];
 
 http.createServer(handleRequest).listen(port);
 console.log('Listening on port:' + port);
@@ -10,30 +11,37 @@ function handleRequest(request, response) {
 	console.log('request.url = ' + request.url);
 
 	let assistant = new Assistant(request, response);
-    let path = assistant.path;
-    let messages = [];
+	let path = assistant.path;
 
 	try {
 		if (path === '/') {
 			assistant.sendFile('index.html');
 		} else if (path === '/chat') {
-			console.log('Parsing the POST');
-			assistant.parsePostParams((params) => {
-				let message = {
-					name: 'Anonymous',
-					body: params.body,
-					when: new Date().toISOString()
-				};
-                messages.push(message);
-                let data = JSON.stringify(messages)
-                let type = mime.lookup('json');
-                assistant.finishResponse(type, data)
-			});
+			console.log(request.method);
+			if (request.method === 'GET') {
+				console.log('Parsing the GET');
+				let data = JSON.stringify(messages);
+				let type = mime.lookup('json');
+				assistant.finishResponse(type, data);
+			} else {
+				console.log('Parsing the POST');
+				assistant.parsePostParams((params) => {
+					let message = {
+						name: 'Anonymous',
+						body: params.body,
+						when: new Date().toISOString()
+					};
+					messages.push(message);
+					let data = JSON.stringify(messages);
+					let type = mime.lookup('json');
+					assistant.finishResponse(type, data);
+				});
+			}
 		} else {
-            let fileName = request.url.slice(1);
-            assistant.sendFile(fileName);
+			let fileName = request.url.slice(1);
+			assistant.sendFile(fileName);
 		}
 	} catch (error) {
-        assistant.sendError(404, 'Error: ' + error.toString())
+		assistant.sendError(404, 'Error: ' + error.toString());
 	}
 }
