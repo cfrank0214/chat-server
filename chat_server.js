1561bfb304c58c47edf5e0ebac48a238a4b0953d
ipointer2 <chris.e.frank@gmail.com>
2018-07-23 13:23:00 -0400

    wip returning

--- chat_server.js
+++ chat_server.js
@@ -1,39 +1,39 @@
-// setup requirements
 const http = require('http');
-const fs = require('fs');
 const mime = require('mime-types');
+const Assistant = require('./assistant');
 const port = process.env.PORT || 5000;
 
 http.createServer(handleRequest).listen(port);
 console.log('Listening on port:' + port);
 
 function handleRequest(request, response) {
-    console.log("Request recieved!");
-    console.log(request.url)
-    let fileName;
-    let data
-    let type = 'text/plain';
+	console.log('request.url = ' + request.url);
 
-    try {
-        if (request.url === '/') {
-            fileName = 'index.html'
+	let assistant = new Assistant(request, response);
+    let path = assistant.path;
+    let messages = [];
 
-        } else {
-            fileName = request.url.slice(1)
-        }
-        data = fs.readFileSync(fileName)
-        type = mime.lookup(fileName)
-        response.statusCode = 200;
-    } catch (error) {
-        console.log(error);
-        data = "Error: " + error.toString();
-        response.statusCode = 404;
-    }
-
-    let contentType = `${type}; charset=utf-8`
-
-    response.setHeader('Content-Type', contentType)
-
-    response.write(data)
-    response.end();
+	try {
+		if (path === '/') {
+			assistant.sendFile('index.html');
+		} else if (path === '/chat') {
+			console.log('Parsing the POST');
+			assistant.parsePostParams((params) => {
+				let message = {
+					name: 'Anonymous',
+					body: params.body,
+					when: new Date().toISOString()
+				};
+                messages.push(message);
+                let data = JSON.stringify(messages)
+                let type = mime.lookup('json');
+                assistant.finishResponse(type, data)
+			});
+		} else {
+            let fileName = request.url.slice(1);
+            assistant.sendFile(fileName);
+		}
+	} catch (error) {
+        assistant.sendError(404, 'Error: ' + error.toString())
+	}
 }
